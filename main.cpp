a43cd17dc6b1db9dbc60ad447d9b75d396e88e1d
unixqq <77384507+unixqq@users.noreply.github.com>
2022-04-24 07:14:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,26 +4,16 @@
 
 char16_t charArray[] = {'q','w','e','r','t','y','u','i','o','f'};
 
-
-    std::string randomString(int length) {
+std::string randomString(int length) {
         
-        std::string s;
-        std::mt19937 rnd(time(0));
-        for (int i = 0; i < length; i++) {
-            s += charArray[rnd() % ((sizeof(charArray) / 2) - 1)];
-            
-        }
-        
-         
-        return (s);
-        
+    std::string s;
+    std::mt19937 rnd(time(0));
+    for (int i = 0; i < length; i++) {
+        s += charArray[rnd() % ((sizeof(charArray) / 2) - 1)];}
+        return (s);        
 }
 
-    int main()
-{
-    
-        
+    int main() { 
         std::cout << randomString(10);
-       
         return 0;
 }
